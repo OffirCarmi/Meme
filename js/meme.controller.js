cda9818d78428cf4bed52e53e83f30a02751853b
Offir Carmi <offircarmi@gmail.com>
2022-04-13 23:22:06 +0300

    ui fixes, input shows live text

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -5,7 +5,8 @@ var gCtx
 const memeFont = new FontFace('impact', 'url(/fonts/Impact.ttf)')
 
 
-function renderCanvas() {
+function renderCanvas(id) {
+    const img = getImgById(id)
     const elCanvas = document.querySelector('canvas')
     elCanvas.width = 495
     elCanvas.height = 495
@@ -21,27 +22,45 @@ function renderMeme() {
 
     const toRender = new Image()
     toRender.src = img.url
-    toRender.onload = function() {
+    toRender.onload = function () {
         gCtx.drawImage(toRender, 0, 0)
     }
+
+    const txt = meme.lines[0].txt
+    // drawText(txt, Math.floor(gElCanvas.width / 2), 50)
+
+    document.querySelector('[name=text]').value = txt
 
 
 
 }
 
-
-// function drawText(lineIdx, txt, x, y) {
-//     // console.log(elImg.width);
-//     // console.log(elImg.height);
-//     gCtx.font = `${gMeme.lines[lineIdx].size}px impact`
-//     gCtx.textAlign = gMeme.lines[lineIdx].align
-//     gCtx.fillStyle = 'white'
-//     gCtx.lineWidth = 2
-//     gCtx.strokeStyle = gMeme.lines[lineIdx].color
-//     gCtx.fillText(txt, x, y)
+// function drawStroked(text, x, y) {
+//     gCtx.font = '80px Sans-serif';
+//     gCtx.strokeStyle = 'black';
+//     gCtx.lineWidth = 8;
+//     gCtx.strokeText(text, x, y);
+//     gCtx.fillStyle = 'white';
+//     gCtx.fillText(text, x, y);
 // }
 
-    // const txt = document.querySelector('[name=txt]').value
-    // console.log(txt);
 
-    // drawText(0, gMeme.lines[0].txt, elImg.width / 2, 55)
+// drawStroked("37°", 50, 150);
+
+
+
+
+
+function drawText(txt, x, y) {
+    const meme = getMeme()
+    // console.log(meme);
+    // console.log(txt);
+    // console.log(x);
+    // console.log(y);
+    gCtx.font = `${meme.lines[0].size}px impact`
+    gCtx.textAlign = meme.lines[0].align
+    gCtx.fillStyle = 'white'
+    // gCtx.lineWidth = 2
+    gCtx.strokeStyle = meme.lines[0].color
+    gCtx.fillText(txt, x, y)
+}
