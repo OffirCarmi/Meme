a6a7856408e5b821757b0b62b645e0a2bae0d806
Offir Carmi <offircarmi@gmail.com>
2022-04-14 13:55:35 +0300

    added 1st line txt (size, align and font features included)

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,45 +1,176 @@
 'use strict'
+const memeFont = new FontFace('impact', 'url(/fonts/Impact.ttf)')
 
 var gElCanvas
 var gCtx
-const memeFont = new FontFace('impact', 'url(/fonts/Impact.ttf)')
+var gTextBorder
+var gXPos
+var gYPos
+
+// var gPos = {x,y}
 
 
 function renderCanvas(id) {
     const img = getImgById(id)
-    const elCanvas = document.querySelector('canvas')
-    elCanvas.width = 495
-    elCanvas.height = 495
+    const meme = getMeme()
     gElCanvas = document.querySelector('canvas')
+    gElCanvas.width = 495
+    gElCanvas.height = 495
     gCtx = gElCanvas.getContext('2d')
+    const elInput = document.querySelector('[name=text]')
+    elInput.addEventListener('keyup', onSetLineTxt)
+
+    document.querySelector('[name=text]').value = meme.lines[0].txt
     renderMeme()
 
 }
 
-function renderMeme() {
-    const meme = getMeme()
-    const img = getImgById(meme.selectedImgIdx)
+// function addListeners() {
+//     addMouseListeners()
+//     addTouchListeners()
+//     window.addEventListener('resize', () => {
+//         resizeCanvas()
+//         renderCanvas()
+//     })
+// }
 
-    const toRender = new Image()
-    toRender.src = img.url
-    toRender.onload = function () {
-        gCtx.drawImage(toRender, 0, 0)
+// function addMouseListeners() {
+//     gElCanvas.addEventListener('mousemove', onMove)
+//     gElCanvas.addEventListener('mousedown', onDown)
+//     gElCanvas.addEventListener('mouseup', onUp)
+// }
+// function addKeyListeners() {
+//     gElCanvas.addEventListener('keyup', onMove)
+//     gElCanvas.addEventListener('touchstart', onDown)
+//     gElCanvas.addEventListener('touchend', onUp)
+// }
+// function addTouchListeners() {
+//     gElCanvas.addEventListener('touchmove', onMove)
+//     gElCanvas.addEventListener('touchstart', onDown)
+//     gElCanvas.addEventListener('touchend', onUp)
+// }
+
+
+function renderMeme() {
+    const memeData = getMeme()
+    const img = getImgById(memeData.selectedImgIdx)
+
+    const meme = new Image()
+    meme.src = img.url
+    meme.onload = function () {
+        gCtx.drawImage(meme, 0, 0)
+        strokedText()
     }
 
+}
 
-    const txt = meme.lines[0].txt
-    addText(txt, Math.floor(gElCanvas.width / 2), 50)
-    document.querySelector('[name=text]').value = txt
+function strokedText() {
+    const memeData = getMeme()
+    const txt = memeData.lines[0].txt
+    const txtWidth = gCtx.measureText(txt).width
+    const canvasWidth = gElCanvas.width
+
+    console.log(txtWidth);
+    console.log(canvasWidth);
+    calcXPos(txt)
+    calcYPos()
+
+    gCtx.font = `${memeData.lines[0].size}px ${memeData.lines[0].font}`
+    gCtx.strokeStyle = memeData.lines[0].strokeColor
+    gCtx.lineWidth = 8
+    gCtx.strokeText(txt, gXPos, gYPos)
+    gCtx.fillStyle = memeData.lines[0].fillColor
+    gCtx.fillText(txt, gXPos, gYPos)
+
 
 }
 
-function addText (txt,x,y){
+function createBorder(txt) {
+    if (!txt) return
+    gTextBorder = { width: (gCtx.measureText(txt).width + 20), height: (memeData.lines[0].size + 15) }
+    gCtx.strokeStyle = 'white'
+    gCtx.lineWidth = 5
+    gCtx.strokeRect(10, 10, gTextBorder.width, gTextBorder.height)
+
+}
+
+function onSetLineTxt() {
+    const txt = document.querySelector('[name=text]').value
+    setLineTxt(txt)
+    renderMeme()
+}
+
+function onBiggerFont() {
+    setBiggerFont()
+    renderMeme()
+}
+
+
+function onSmallerFont() {
+    setSmallerFont()
+    renderMeme()
+}
+
+function onSetAlign(val) {
+    setAlign(val)
+    renderMeme()
+
+}
+
+function calcXPos(txt) {
+    const txtWidth = gCtx.measureText(txt).width
+    const canvasWidth = gElCanvas.width
     const meme = getMeme()
-    const img = getImgById(meme.selectedImgIdx)
-    gCtx.fillStyle = 'white'
-    gCtx.font = '40px impact'
-    gCtx.fillText (txt,x,y)
+    switch (meme.lines[0].align) {
+        case 'center':
+            gXPos = (canvasWidth - txtWidth) / 2
+            break
+        case 'left':
+            gXPos = 20
+            break
+        case 'right':
+            gXPos = (canvasWidth - txtWidth) - 20
+            break
+    }
+
 }
+
+function calcYPos() {
+    const meme = getMeme()
+    gYPos = meme.lines[0].size + 15
+
+}
+
+
+
+function OnSetFont(font) {
+    setFont(font)
+    renderMeme()
+
+}
+
+// function openColorPicker(elBtn) {
+// console.log(elBtn);
+// const strHtml = '<input type="color"></input>'
+// elBtn.innerHTML = strHtml
+// }
+
+// function addText(txt, x, y) {
+//     const meme = getMeme()
+//     const img = getImgById(meme.selectedImgIdx)
+//     gCtx.fillStyle = 'white'
+//     gCtx.font = '40px impact'
+//     gCtx.fillText(txt, x, y)
+// }
+
+
+// function downloadCanvas(elLink) {
+//     const data = gElCanvas.toDataURL()
+//     elLink.href = data
+//     elLink.download = 'memegen'
+// }
+
+
 
 // function drawStroked(text, x, y) {
 //     gCtx.font = '80px Sans-serif';
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,7 +3,7 @@
 var gMeme = {
     selectedImgIdx: null,
     selectedLineIdx: 0,
-    lines: [{ txt: 'Edit text', size: 50, align: 'center', color: 'black', font:'impact' }]
+    lines: [{ txt: 'Edit text', size: 50, align: 'left', strokeColor: 'black', fillColor: 'white', lineWidth: 5, font: 'impact' }]
 }
 
 
@@ -12,10 +12,34 @@ function updateImgIdx(id) {
 
 }
 
-function getMeme(){
+function getMeme() {
     return gMeme
 }
 
+function setLineTxt(txt) {
+    gMeme.lines[0].txt = txt
+}
+
+function setBiggerFont() {
+    gMeme.lines[0].size += 5
+    gMeme.lines[0].lineWidth += 5
+}
+
+
+function setSmallerFont() {
+    gMeme.lines[0].size -= 5
+    gMeme.lines[0].lineWidth -= 5
+
+}
+
+function setAlign(val) {
+    gMeme.lines[0].align = val
+}
+
+function setFont(font) {
+    gMeme.lines[0].font = font
+
+}
 
 
 
