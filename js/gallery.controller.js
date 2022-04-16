356a7abce0315769db872dee4f3c56dd090c4d6d
Offir Carmi <offircarmi@gmail.com>
2022-04-16 13:34:16 +0300

    resonisive esign

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -11,21 +11,29 @@ function renderGallery() {
         return `<img onclick="onImgSelect(${img.id})" src="${img.url}">`
     })
     elGallery.innerHTML = strHTML.join('')
+
 }
 
 function onImgSelect(id) {
     const elGallery = document.querySelector('.gallery')
     elGallery.style.display = 'none'
+    document.querySelector('.gallery-link').classList.remove('active')
     const elMeme = document.querySelector('.meme')
     elMeme.style.display = 'block'
+
     updateImgIdx(id)
     renderCanvas()
 }
 
 function showGallery(elLink) {
-    elLink.active = true
+    document.querySelector('.gallery-link').classList.add('active')
     const elGallery = document.querySelector('.gallery')
     const elMeme = document.querySelector('.meme')
     elGallery.style.display = 'block'
     elMeme.style.display = 'none'
+}
+
+function toggleMenu() {
+    document.body.classList.toggle ('menu-open')
+
 }
